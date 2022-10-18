c2370ef84c67dadf2b32916bc37a87d26f293d27
Роман <rvfirstov@gmail.com>
2022-10-18 15:09:19 +0300

    добавил postgres и liquibase

--- src/main/java/com/example/restservice/model/Employees.java
+++ src/main/java/com/example/restservice/model/Employees.java
@@ -11,7 +11,7 @@ import lombok.Data;
 public class Employees {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String name;
