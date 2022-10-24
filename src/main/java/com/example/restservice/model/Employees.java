608a7449e372a87076ca333be833ba5ffa2ae142
Роман <rvfirstov@gmail.com>
2022-10-24 17:41:44 +0300

    добавил указание table и column

--- src/main/java/com/example/restservice/model/Employees.java
+++ src/main/java/com/example/restservice/model/Employees.java
@@ -1,20 +1,26 @@
 package com.example.restservice.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "employees")
 public class Employees {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id")
   private Long id;
 
+  @Column(name = "name")
   private String name;
 
+  @Column(name = "age")
   private Integer age;
 }
