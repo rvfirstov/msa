ea79cc3841704cf6f7f5c1894576637ed809558c
Роман <rvfirstov@gmail.com>
2022-10-24 20:55:38 +0300

    добавил h2

---
+++ src/test/java/com/example/restservice/service/EmployeesServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.restservice.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeesServiceTest {
+
+  @Test
+  void getAll() {
+  }
+
+  @Test
+  void getById() {
+  }
+
+  @Test
+  void save() {
+  }
+
+  @Test
+  void delete() {
+  }
+}
